7d6fc7c35ea8e5a4655864e679bb379de7c3d0f4
Edson Tessaro <edsonfast@gmail.com>
2016-04-26 13:44:47 -0300

    Layout

--- app/src/main/java/br/com/fast/edson/sunshine/MainActivity.java
+++ app/src/main/java/br/com/fast/edson/sunshine/MainActivity.java
@@ -12,16 +12,21 @@ import android.view.MenuItem;
 public class MainActivity extends AppCompatActivity {
 
     private final String LOG_TAG = MainActivity.class.getSimpleName();
+    private final String FORECASTFRAGMENT_TAG = "FFTAG";
+
+    private String mLocation;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        mLocation = Utility.getPreferredLocation(this);
+
         setContentView(R.layout.activity_main);
-//        if (savedInstanceState == null) {
+        if (savedInstanceState == null) {
 //            getSupportFragmentManager().beginTransaction()
-//                    .add(R.id.container, new ForecastFragment())
+//                    .add(R.id.container, new ForecastFragment(), FORECASTFRAGMENT_TAG)
 //                    .commit();
-//        }
+        }
     }
 
     @Override
@@ -64,6 +69,20 @@ public class MainActivity extends AppCompatActivity {
             startActivity(intent);
         } else {
             Log.d(LOG_TAG, "Couldn't call " + location + ", no receiving apps installed!");
+        }
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        String location = Utility.getPreferredLocation( this );
+        // update the location in our second pane using the fragment manager
+        if (location != null && !location.equals(mLocation)) {
+            ForecastFragment ff = (ForecastFragment)getSupportFragmentManager().findFragmentByTag(FORECASTFRAGMENT_TAG);
+            if ( null != ff ) {
+                ff.onLocationChanged();
+            }
+            mLocation = location;
         }
     }
 
